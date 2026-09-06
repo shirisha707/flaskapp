e2955eff8cf30010f13d0fedd48efc81806a4034
shirisha707 <anneboinashirisha0@gmail.com>
2026-09-06 21:13:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-   return " " "
+   return """
    <html>
      <head>
      </head>
@@ -10,6 +10,6 @@ def home():
      <h1>Hello Rgukt</h1>
      </body>
      </html>
-     " " "
+     """
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=5000)       
